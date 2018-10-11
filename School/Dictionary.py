eb0ae54e190ed430fee4cf54ac3698a91c2eea12
jesse <jesse.baan@student.hu.nl>
2018-10-11 12:49:55 +0100

    1st test

---
+++ School/Dictionary.py
@@ -0,0 +1,23 @@
+
+
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": " August",
+    "sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+print(monthConversions.get("Luv", "Not a valid key"))
+
+
+
+
+
---
+++ School/Test.py
@@ -0,0 +1,26 @@
+
+num1 = float(input("enter first number: "))
+OP = input("enter operator: ")
+num2 = float(input("enter second number: "))
+
+if OP == "+":
+    print(num1 + num2)
+elif OP == "-":
+    print(num1 - num2)
+elif OP == "/":
+    print(num1 / num2)
+elif OP == "*":
+    print(num1 * num2)
+else:
+    print("invalid operator")
+
+
+
+
+
+
+
+
+
+
+
